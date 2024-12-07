205a8fbd9a04dfe089871c4c836f3a022de61d42
Alexandr Gromov <alex.gromov.2410@gmail.com>
2024-12-06 20:40:51 -0800

    Print vec3 and added operator+ overload

--- Game/src/Math/Vector3.h
+++ Game/src/Math/Vector3.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <format>
 
 namespace Game {
 
@@ -9,6 +10,9 @@ namespace Game {
 		static vec3 Normalize(const vec3& v)
 		{
 			const float length = std::hypot(v.x, v.y, v.z);
+			if (length == 0.0f)
+				return { 0.0f, 0.0f, 0.0f };
+
 			return { .x = v.x / length, .y = v.y / length, .z = v.z / length };
 		}
 
@@ -27,6 +31,21 @@ namespace Game {
 		float y;
 		float z;
 	};
+
+	constexpr vec3& operator+=(vec3& v1, const vec3& v2)
+	{
+		v1.x += v2.x;
+		v1.y += v2.y;
+		v1.z += v2.z;
+
+		return v1;
+	}
+
+	constexpr vec3 operator+(const vec3& v1, const vec3& v2)
+	{
+		vec3 temp = v1;
+		return temp += v2;
+	}
 
 	constexpr vec3& operator-=(vec3& v1, const vec3& v2)
 	{
@@ -49,3 +68,17 @@ namespace Game {
 	}
 
 }
+
+template<>
+struct std::formatter<Game::vec3>
+{
+	constexpr auto parse(std::format_parse_context& ctx)
+	{
+		return std::begin(ctx);
+	}
+
+	auto format(const Game::vec3& obj, std::format_context& ctx) const
+	{
+		return std::format_to(ctx.out(), "x={} y={} z={}", obj.x, obj.y, obj.z);
+	}
+};
