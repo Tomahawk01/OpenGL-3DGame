103d2e703c566e2fe721b4acae35025be8a77a7f
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-03-26 21:20:11 -0700

    Added Dot() to vec4

--- GameLib/src/Math/Vector4.h
+++ GameLib/src/Math/Vector4.h
@@ -9,17 +9,19 @@ namespace Game {
 	{
 		constexpr vec4()
 			: vec4(0.0f)
-		{
-		}
+		{}
 
 		constexpr vec4(float xyzw)
 			: vec4(xyzw, xyzw, xyzw, xyzw)
-		{
-		}
+		{}
 
 		constexpr vec4(float x, float y, float z, float w)
 			: x(x), y(y), z(z), w(w)
+		{}
+
+		static constexpr float Dot(const vec4& v1, const vec4& v2)
 		{
+			return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w;
 		}
 
 		constexpr bool operator==(const vec4&) const = default;
