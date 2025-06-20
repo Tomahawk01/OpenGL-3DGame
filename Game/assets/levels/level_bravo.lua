1adf19a1caba5ff22ad078d7cd83c90dccd3a154
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-06-19 23:39:25 -0700

    Updated lua scripts

--- Game/assets/levels/level_bravo.lua
+++ Game/assets/levels/level_bravo.lua
@@ -1,9 +1,9 @@
 barrels = {}
 last_player_position = vec3(0.0, 0.0, 0.0)
-original_player_position = vec3(0.0, 0.0, 0.0)
+original_last_player_position = vec3(0.0, 0.0, 0.0)
 
 function init_level(player_position)
-	original_player_position = player_position
+	original_last_player_position = player_position
 	restart_level()
 end
 
@@ -23,7 +23,7 @@ function restart_level()
 		position = vec3(5.0, 0.0, 0.0),
 		visibility = true
 	}
-	last_player_position = original_player_position
+	last_player_position = original_last_player_position
 end
 
 function is_complete()
--- Game/assets/levels/level_charlie.lua
+++ Game/assets/levels/level_charlie.lua
@@ -1,9 +1,9 @@
 barrels = {}
 last_player_position = vec3(0.0, 0.0, 0.0)
-original_player_position = vec3(0.0, 0.0, 0.0)
+original_last_player_position = vec3(0.0, 0.0, 0.0)
 
 function init_level(player_position)
-	original_player_position = player_position
+	original_last_player_position = player_position
 	restart_level()
 end
 
@@ -22,27 +22,35 @@ function restart_level()
 		position = vec3(0.0, 0.0, 5.0),
 		visibility = true,
 		color = vec3(1.0, 0.0, 0.0),
-		tint = 1.0
+		tint = 1.0,
+		collision_layer = 1,
+		collision_mask = 0,
 	}
 	barrels[2] = {
 		position = vec3(5.0, 0.0, 0.0),
 		visibility = true,
 		color = vec3(1.0, 0.0, 0.0),
-		tint = 0.5
+		tint = 0.5,
+		collision_layer = 2,
+		collision_mask = 2,
 	}
     barrels[3] = {
 		position = vec3(0.0, 0.0, -5.0),
 		visibility = true,
 		color = vec3(0.0, 0.0, 1.0),
-		tint = 1.0
+		tint = 1.0,
+		collision_layer = 1,
+		collision_mask = 0,
 	}
 	barrels[4] = {
 		position = vec3(-5.0, 0.0, 0.0),
 		visibility = true,
 		color = vec3(0.0, 0.0, 1.0),
-		tint = 0.5
+		tint = 0.5,
+		collision_layer = 2,
+		collision_mask = 2,
 	}
-	last_player_position = original_player_position
+	last_player_position = original_last_player_position
 end
 
 function is_complete()
@@ -55,7 +63,11 @@ function barrel_count()
 end
 
 function barrel_info(index)
-	return barrels[index].position, barrels[index].color, barrels[index].tint
+	return barrels[index].position,
+		   barrels[index].color,
+		   barrels[index].tint,
+		   barrels[index].collision_layer,
+		   barrels[index].collision_mask
 end
 
 function set_barrel_visibility(index, visibility)
