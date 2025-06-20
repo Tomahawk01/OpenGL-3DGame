95a15f52a94f9b489e69d7affa99e4aa0a756e4f
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-06-20 00:33:19 -0700

    Updated lua levels

--- Game/assets/levels/level_bravo.lua
+++ Game/assets/levels/level_bravo.lua
@@ -17,11 +17,19 @@ end
 function restart_level()
 	barrels[1] = {
 		position = vec3(0.0, 0.0, 0.0),
-		visibility = true
+		visibility = true,
+		color = vec3(0.0, 0.0, 1.0),
+		tint = 1.0,
+		collision_layer = 1,
+		collision_mask = 0,
 	}
 	barrels[2] = {
 		position = vec3(5.0, 0.0, 0.0),
-		visibility = true
+		visibility = true,
+		color = vec3(0.0, 0.0, 1.0),
+		tint = 0.5,
+		collision_layer = 2,
+		collision_mask = 2,
 	}
 	last_player_position = original_last_player_position
 end
@@ -34,10 +42,18 @@ function barrel_count()
 	return #barrels
 end
 
-function barrel_position(index)
-	return barrels[index].position
+function barrel_info(index)
+	return barrels[index].position,
+		   barrels[index].color,
+		   barrels[index].tint,
+		   barrels[index].collision_layer,
+		   barrels[index].collision_mask
 end
 
 function set_barrel_visibility(index, visibility)
 	barrels[index].visibility = visibility
+end
+
+function set_barrel_position(index, position)
+	barrels[index].position = position
 end
