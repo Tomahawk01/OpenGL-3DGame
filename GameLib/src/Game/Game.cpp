2e0225b671c93dde6b53433c753e3809edfbdec6
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-07-15 16:27:00 -0700

    WIP Integrated routines
    - level routine is broken

--- GameLib/src/Game/Game.cpp
+++ GameLib/src/Game/Game.cpp
@@ -24,10 +24,12 @@
 #include "TLV/Utilities.h"
 
 #include "Game/Player.h"
+#include "Game/Routines/InputRoutine.h"
+#include "Game/Routines/LevelRoutine.h"
+#include "Game/Routines/RenderRoutine.h"
 
 #include <iostream>
 #include <print>
-#include <numbers>
 #include <cmath>
 #include <type_traits>
 #include <ranges>
@@ -35,46 +37,6 @@
 #include <random>
 
 namespace {
-
-	bool IntersectsFrustum(const Game::TransformedShape& boundingBox, const std::array<Game::FrustumPlane, 6u>& planes)
-	{
-		Game::Expect(boundingBox.GetShape()->GetType() == Game::ShapeType::BOX, "This is not a box");
-
-		const auto* boxShape = static_cast<const Game::BoxShape*>(boundingBox.GetShape());
-
-		const auto position = boundingBox.GetTransform().Position;
-		const auto min = position - boxShape->GetDimensions();
-		const auto max = position + boxShape->GetDimensions();
-
-		for (const auto& plane : planes)
-		{
-			Game::vec3 positiveVertex = min;
-			if (plane.normal.x >= 0)
-				positiveVertex.x = max.x;
-			if (plane.normal.y >= 0)
-				positiveVertex.y = max.y;
-			if (plane.normal.z >= 0)
-				positiveVertex.z = max.z;
-
-			if (Game::vec3::Dot(plane.normal, positiveVertex) + plane.distance < 0.0f)
-				return false;
-		}
-
-		return true;
-	}
-
-	Game::Camera CreateCamera(const Game::Window& window)
-	{
-		return {
-			{0.0f, 5.0f, 30.0f},
-			{0.0f, 0.0f, 0.0f},
-			{0.0f, 1.0f, 0.0f},
-			std::numbers::pi_v<float> / 4.0f,
-			static_cast<float>(window.GetWidth()),
-			static_cast<float>(window.GetHeight()),
-			0.1f, 1000.0f
-		};
-	}
 
 	uint32_t GetArg(std::string_view argName, const std::vector<std::string_view>& args)
 	{
@@ -99,22 +61,9 @@ namespace Game {
 
 	Game::Game(const std::vector<std::string_view>& args)
 		: m_Running{ true }
-		, m_LevelNames{
-			"level_alpha.lua",
-			"level_foxtrot.lua",
-			"level_echo.lua",
-			"level_bravo.lua",
-			"level_charlie.lua",
-			"level_delta.lua",
-		}
-		, m_Level{}
-		, m_LevelNum{ 0u }
 		, m_Bus{}
 		, m_Window{ 1280u, 720u, GetArg("-x", args), GetArg("-y", args) }
-		, m_Player{ m_Bus, CreateCamera(m_Window) }
-	{
-		m_Bus.Subscribe(MessageType::LEVEL_COMPLETE, this);
-	}
+	{}
 
 	void Game::Run(std::string_view resourceRoot)
 	{
@@ -158,79 +107,17 @@ namespace Game {
 			}, sampler);
 		resourceCache.Insert<Mesh>("floor", meshLoader.Cube());
 
-		const Renderer renderer{ reader, meshLoader, m_Window.GetWidth(), m_Window.GetHeight() };
+		Scheduler scheduler{};
 
-		float gamma = 2.2f;
+		InputRoutine inputRoutine{ m_Window, m_Bus, scheduler };
+		LevelRoutine levelRoutine{ m_Window, m_Bus, scheduler, resourceCache, reader };
+		RenderRoutine renderRoutine{ levelRoutine, m_Window, m_Bus, scheduler, meshLoader, reader };
 
-		WireframeRenderer wireframeRenderer{};
+		scheduler.Add(inputRoutine.CreateTask());
+		scheduler.Add(levelRoutine.CreateTask());
+		scheduler.Add(renderRoutine.CreateTask());
 
-		auto currentLevel = m_LevelNum;
-
-		m_Level = std::make_unique<LuaLevel>(m_LevelNames[m_LevelNum], resourceCache, reader, m_Player, m_Bus);
-		m_Window.SetTitle(m_LevelNames[m_LevelNum]);
-
-		while (m_Running)
-		{
-			if (currentLevel != m_LevelNum)
-			{
-				m_Player.Restart();
-				m_Level.reset(nullptr);
-				m_Level = std::make_unique<LuaLevel>(m_LevelNames[m_LevelNum], resourceCache, reader, m_Player, m_Bus);
-				currentLevel = m_LevelNum;
-
-				m_Window.SetTitle(m_LevelNames[m_LevelNum]);
-			}
-
-			Expect(m_Level, "Level cannot be nullptr");
-
-			auto event = m_Window.PollEvent();
-			while (event && m_Running)
-			{
-				std::visit([&](auto&& arg)
-				{
-					using T = std::decay_t<decltype(arg)>;
-
-					if constexpr (std::same_as<T, StopEvent>)
-					{
-						m_Running = false;
-					}
-					else if constexpr (std::same_as<T, KeyEvent>)
-					{
-						if (arg.GetKey() == Key::ESC)
-						{
-							m_Running = false;
-						}
-
-						m_Bus.PostKeyPress(arg);
-					}
-					else if constexpr (std::same_as<T, MouseEvent>)
-					{
-						m_Bus.PostMouseMove(arg);
-					}
-				}, *event);
-				event = m_Window.PollEvent();
-			}
-
-			m_Player.Update();
-			m_Level->Update(m_Player);
-
-			for (auto& entity : m_Level->GetScene().entities)
-			{
-				entity->SetVisibility(IntersectsFrustum(entity->GetBoundingBox(), m_Player.GetCamera().FrustumPlanes()));
-			}
-
-			wireframeRenderer.Draw(m_Player.GetCamera());
-
-			renderer.Render(m_Player.GetCamera(), m_Level->GetScene(), gamma);
-
-			m_Window.Swap();
-		}
-	}
-
-	void Game::HandleLevelComplete(std::string_view levelName)
-	{
-		Logger::Info("Level complete: {}", levelName);
-		m_LevelNum = (m_LevelNum + 1) % m_LevelNames.size();
+		scheduler.Run();
 	}
 
 }
--- GameLib/src/Game/Game.h
+++ GameLib/src/Game/Game.h
@@ -3,9 +3,6 @@
 #include "Messaging/Subscriber.h"
 #include "Core/Window.h"
 
-#include "Game/Levels/LuaLevel.h"
-#include "Game/Player.h"
-
 #include <string_view>
 #include <memory>
 
@@ -18,16 +15,10 @@ namespace Game {
 
 		void Run(std::string_view resourceRoot);
 
-		void HandleLevelComplete(std::string_view levelName) override;
-
 	private:
 		bool m_Running;
-		std::vector<std::string> m_LevelNames;
-		std::unique_ptr<LuaLevel> m_Level;
-		size_t m_LevelNum;
 		MessageBus m_Bus;
 		Window m_Window;
-		Player m_Player;
 	};
 
 }
