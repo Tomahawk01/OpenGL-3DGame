8ab296a825de66f9e7b1d06f3e2d6fb23ddeb836
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-07-27 02:08:16 -0700

    Disable frustum culling in renderer

--- GameLib/src/Renderer/Renderer.cpp
+++ GameLib/src/Renderer/Renderer.cpp
@@ -119,7 +119,7 @@ namespace Game {
 
 		::glDepthMask(GL_TRUE);
 
-		for (const Entity* entity : scene.entities | std::views::filter([](const auto* e) { return e->IsVisible(); }))
+		for (const Entity* entity : scene.entities)// | std::views::filter([](const auto* e) { return e->IsVisible(); }))
 		{
 			const Mesh* mesh = entity->GetMesh();
 			const Material* material = entity->GetMaterial();
