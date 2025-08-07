23c26a264f0deba76abfac8b5f36094d2a3692ca
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-08-06 17:47:53 -0700

    Set some default sampler options

--- GameLib/src/Renderer/Sampler.cpp
+++ GameLib/src/Renderer/Sampler.cpp
@@ -6,6 +6,9 @@ namespace Game {
 		: m_Handle{ 0u, [](auto sampler) { ::glDeleteSamplers(1, &sampler); } }
 	{
 		::glCreateSamplers(1, &m_Handle);
+
+		::glSamplerParameteri(m_Handle, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		::glSamplerParameteri(m_Handle, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	}
 
 	::GLuint Sampler::GetNativeHandle() const
