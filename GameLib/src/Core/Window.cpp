3d6aef42245a597fceb057f16ba9a86a1b6eb993
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-08-06 17:47:36 -0700

    Enabled OpenGL blending

--- GameLib/src/Core/Window.cpp
+++ GameLib/src/Core/Window.cpp
@@ -269,6 +269,8 @@ namespace Game {
 		SetupDebug();
 
 		::glEnable(GL_DEPTH_TEST);
+		::glEnable(GL_BLEND);
+		::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
 	std::optional<Event> Window::PollEvent() const
