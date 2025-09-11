7892238639af55230f5beb6de4191ae6bb36175b
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-09-10 17:55:31 -0700

    Added ability to control sampler settings

--- GameLib/src/Renderer/Sampler.cpp
+++ GameLib/src/Renderer/Sampler.cpp
@@ -1,15 +1,40 @@
 #include "Sampler.h"
 
+#include "Utilities/Error.h"
+#include "Utilities/Exception.h"
+#include "Utilities/Formatter.h"
+
+namespace {
+
+	::GLenum ToOpenGL(Game::FilterType filterType)
+	{
+		switch (filterType)
+		{
+			case Game::FilterType::LINEAR_MIPMAP: return GL_LINEAR_MIPMAP_LINEAR;
+			case Game::FilterType::LINEAR: return GL_LINEAR;
+			case Game::FilterType::NEAREST: return GL_NEAREST;
+		}
+
+		throw Game::Exception("Unknown filerType: {}", filterType);
+	}
+
+}
+
 namespace Game {
 
-	Sampler::Sampler()
+	Sampler::Sampler(FilterType minFilter, FilterType magFilter, std::optional<float> anisotropySamples)
 		: m_Handle{ 0u, [](auto sampler) { ::glDeleteSamplers(1, &sampler); } }
 	{
 		::glCreateSamplers(1, &m_Handle);
 
-		::glSamplerParameteri(m_Handle, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		::glSamplerParameteri(m_Handle, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		::glSamplerParameterf(m_Handle, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.0f);
+		::glSamplerParameteri(m_Handle, GL_TEXTURE_MIN_FILTER, ToOpenGL(minFilter));
+		::glSamplerParameteri(m_Handle, GL_TEXTURE_MAG_FILTER, ToOpenGL(magFilter));
+
+		if (anisotropySamples)
+		{
+			Expect(*anisotropySamples >= 1.0f, "Invalid samples: {}", *anisotropySamples);
+			::glSamplerParameterf(m_Handle, GL_TEXTURE_MAX_ANISOTROPY_EXT, *anisotropySamples);
+		}
 	}
 
 	::GLuint Sampler::GetNativeHandle() const
--- GameLib/src/Renderer/Sampler.h
+++ GameLib/src/Renderer/Sampler.h
@@ -3,17 +3,38 @@
 #include "Utilities/AutoRelease.h"
 #include "Renderer/OpenGL.h"
 
+#include <string>
+#include <optional>
+
 namespace Game {
+
+	enum class FilterType
+	{
+		LINEAR_MIPMAP,
+		LINEAR,
+		NEAREST
+	};
 
 	class Sampler
 	{
 	public:
-		Sampler();
+		Sampler(FilterType minFilter, FilterType magFilter, std::optional<float> anisotropySamples = std::nullopt);
 
 		::GLuint GetNativeHandle() const;
 
 	private:
 		AutoRelease<::GLuint> m_Handle;
 	};
+
+	inline std::string to_string(FilterType filterType)
+	{
+		switch (filterType)
+		{
+			case FilterType::LINEAR_MIPMAP: return "LINEAR_MIPMAP";
+			case FilterType::LINEAR: return "LINEAR";
+			case FilterType::NEAREST: return "NEAREST";
+			default: return "unknown";
+		}
+	}
 
 }
